e5b1f653ea26a506407b5d4143ffefcd7bc8ea42
sameer kumar <samkum199@gmail.com>
2021-06-03 11:27:02 +0530

    initial commit

---
+++ tempapp/tempreact/src/components/Tempapp.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./css/style.css";
+
+const Tempapp = () => {
+    return(
+        <>
+        <div className="box">
+        <div className="inputData">
+            <input
+            type="search"
+            className="inputFeild" 
+            onChange={ (event) => {  
+
+            }
+            } />
+        </div>
+
+        </div>
+        </>
+    )
+}
+
+export default Tempapp;
